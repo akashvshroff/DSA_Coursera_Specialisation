12b6b49e83a819bb98a61e43fc1ddc1d9d86db94
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-08-15 16:13:19 +0530

    Week 5 - Partial

--- Algorithmic_Toolbox/week5_assignment/primitive_calculator.py
+++ Algorithmic_Toolbox/week5_assignment/primitive_calculator.py
@@ -1,6 +1,7 @@
 # Uses python3
 import sys
 
+
 def optimal_sequence(n):
     sequence = []
     while n >= 1:
@@ -13,9 +14,11 @@ def optimal_sequence(n):
             n = n - 1
     return reversed(sequence)
 
-input = sys.stdin.read()
-n = int(input)
-sequence = list(optimal_sequence(n))
-print(len(sequence) - 1)
-for x in sequence:
-    print(x, end=' ')
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    n = int(input)
+    sequence = list(optimal_sequence(n))
+    print(len(sequence) - 1)
+    for x in sequence:
+        print(x, end=' ')
