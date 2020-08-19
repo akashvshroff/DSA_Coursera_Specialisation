6109e31ad4aa06f758f704b166c2b8b8a3902817
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-08-19 12:32:49 +0530

    Week 6 - Partial

--- Algorithmic_Toolbox/week6_assignment/partition3.py
+++ Algorithmic_Toolbox/week6_assignment/partition3.py
@@ -10,7 +10,6 @@ def optimal_weight(W, w):
     althought that would use the same approach only replacing the w[i] marked
     by an asterisk by v[i] where v[i] corresponds to the value of the ith item.
     """
-    count = 0
     n = len(w)
     if not W or not n:
         return 0
@@ -23,12 +22,22 @@ def optimal_weight(W, w):
                 val = value[i-1][weight-w[i]] + w[i]  # * Replace with v[i]
                 if value[i][weight] < val:
                     value[i][weight] = val
-            if value[i][weight] == W:
-                count += 1
-    if count < 3:
-        return 0
-    else:
-        return 1
+
+    items_chosen = []
+    i = len(w) - 1
+    j = W
+
+    while i and j:
+        if value[i][j] != value[i-1][j]:  # item has been chosen
+            items_chosen.append(i)
+            j -= w[i]
+        i -= 1
+
+    if items_chosen:
+        for item in items_chosen:
+            w.pop(item)
+    w.pop(0)  # filler element added
+    return value[-1][-1], w
 
 
 def partition3(A, k=3):
@@ -36,7 +45,12 @@ def partition3(A, k=3):
     if not A or not total or total % k:
         return 0
     bucket_sum = total//k
-    return optimal_weight(bucket_sum, A)
+    w1, A = optimal_weight(bucket_sum, A)
+    w2, A = optimal_weight(bucket_sum, A)
+    if w1 == bucket_sum and w2 == bucket_sum:
+        return 1
+    else:
+        return 0
 
 
 if __name__ == '__main__':
--- Algorithmic_Toolbox/week6_assignment/partition_backtracking.py
+++ Algorithmic_Toolbox/week6_assignment/partition_backtracking.py
@@ -1,4 +1,10 @@
-def partition3():
+def partition3(n, nums):
     """
     Another approach to solve the 3-partition problem using backtracking.
     """
+
+
+if __name__ == "__main__":
+    n = int(input())
+    nums = list(map(int, input().split()))
+    print(partition3(n, nums))
