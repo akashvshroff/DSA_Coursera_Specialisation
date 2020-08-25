57fd61bae5a32113d8621285b23c2d593ac9f858
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-08-25 10:03:01 +0530

    Week 1 - Partial

--- Data_Structures/week1_assignment/check_brackets.py
+++ Data_Structures/week1_assignment/check_brackets.py
@@ -1,30 +1,39 @@
 # python3
 
-from collections import namedtuple
-
-Bracket = namedtuple("Bracket", ["char", "position"])
-
 
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
 
 def find_mismatch(text):
-    opening_brackets_stack = []
+    """
+    Check if all parentheses are balanced, if not return the index position
+    (1-based) where the error lies with priority given to closing brackets and
+    then opening ones.
+    """
+    br_stack = []
+    id_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
+            br_stack.append(next)
+            id_stack.append(i+1)
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if br_stack and are_matching(br_stack[-1], next):
+                br_stack.pop()
+                id_stack.pop()
+            else:
+                return i + 1
+    if br_stack:
+        return id_stack[0]
+    return "Success"
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
