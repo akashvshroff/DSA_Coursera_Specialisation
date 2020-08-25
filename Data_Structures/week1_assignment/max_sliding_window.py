3e734869f12160d6a7e7316d3f8531acd84cf7e3
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-08-25 11:51:08 +0530

    Week 1 - Partial

--- Data_Structures/week1_assignment/max_sliding_window.py
+++ Data_Structures/week1_assignment/max_sliding_window.py
@@ -1,18 +1,64 @@
 # python3
+class Queue:
+    def __init__(self):
+        """
+        Implements a queue with 2 stacks.
+        """
+        self.s1 = []
+        self.s2 = []
+        self.max_stack = []
+
+    def enqueue(self, val):
+        self.s1.append(val)
+
+    def swap_stacks(self):
+        """
+        Puts all the items of stack 1 into stack 2 allowing you to thereby make
+        a queue.
+        """
+        if not self.s2:
+            while self.s1:
+                item = self.s1.pop()
+                self.s2.append(item)
+                if self.max_stack:
+                    m_s_f = self.max_stack[-1]
+                    if item > m_s_f:
+                        self.max_stack.append(item)
+                    else:
+                        self.max_stack.append(m_s_f)
+                else:
+                    self.max_stack.append(item)
+
+    def dequeue(self):
+        self.swap_stacks()
+        self.s2.pop()
+        self.max_stack.pop()
+
+    def print_max(self):
+        self.swap_stacks()
+        assert(self.max_stack)
+        print(self.max_stack[-1], end=" ")
 
 
-def max_sliding_window_naive(sequence, m):
-    maximums = []
-    for i in range(len(sequence) - m + 1):
-        maximums.append(max(sequence[i:i + m]))
+def max_sliding_window(n, arr, k):
+    """
+    Solving the maximum sliding window problem using 2 stacks and a max_stack.
+    """
+    queue = Queue()
+    for num in arr[:k]:
+        queue.enqueue(num)
+    queue.print_max()
+    num = k
+    for window in range(n-k):
+        queue.dequeue()
+        queue.enqueue(arr[num])
+        queue.print_max()
+        num += 1
 
-    return maximums
 
 if __name__ == '__main__':
     n = int(input())
     input_sequence = [int(i) for i in input().split()]
     assert len(input_sequence) == n
     window_size = int(input())
-
-    print(*max_sliding_window_naive(input_sequence, window_size))
-
+    max_sliding_window(n, input_sequence, window_size)
--- Data_Structures/week1_assignment/stack_with_max.py
+++ Data_Structures/week1_assignment/stack_with_max.py
@@ -1,20 +1,35 @@
-#python3
+# python3
 import sys
+
 
 class StackWithMax():
     def __init__(self):
+        """
+        Stack implementation which uses an auxilliary stack that maintains the
+        maximum value so far.
+        """
         self.__stack = []
+        self.max_stack = []
 
     def Push(self, a):
         self.__stack.append(a)
+        if not self.max_stack:
+            self.max_stack.append(a)
+        else:
+            m_s_f = self.max_stack[-1]  # max so far
+            if a > m_s_f:
+                self.max_stack.append(a)
+            else:
+                self.max_stack.append(m_s_f)
 
     def Pop(self):
         assert(len(self.__stack))
         self.__stack.pop()
+        self.max_stack.pop()
 
     def Max(self):
         assert(len(self.__stack))
-        return max(self.__stack)
+        return self.max_stack[-1]
 
 
 if __name__ == '__main__':
