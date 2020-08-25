b33e653239b540b2d7ff525dfa2fb5061c7ba3d7
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-08-25 13:48:40 +0530

    Week 1 - Partial

--- Data_Structures/week1_assignment/max_sliding_window.py
+++ Data_Structures/week1_assignment/max_sliding_window.py
@@ -1,59 +1,27 @@
 # python3
-class Queue:
-    def __init__(self):
-        """
-        Implements a queue with 2 stacks.
-        """
-        self.s1 = []
-        self.s2 = []
-        self.max_stack = []
-
-    def enqueue(self, val):
-        self.s1.append(val)
-
-    def swap_stacks(self):
-        """
-        Puts all the items of stack 1 into stack 2 allowing you to thereby make
-        a queue.
-        """
-        if not self.s2:
-            while self.s1:
-                item = self.s1.pop()
-                self.s2.append(item)
-                if self.max_stack:
-                    m_s_f = self.max_stack[-1]
-                    if item > m_s_f:
-                        self.max_stack.append(item)
-                    else:
-                        self.max_stack.append(m_s_f)
-                else:
-                    self.max_stack.append(item)
-
-    def dequeue(self):
-        self.swap_stacks()
-        self.s2.pop()
-        self.max_stack.pop()
-
-    def print_max(self):
-        self.swap_stacks()
-        assert(self.max_stack)
-        print(self.max_stack[-1], end=" ")
+from collections import deque
 
 
 def max_sliding_window(n, arr, k):
     """
-    Solving the maximum sliding window problem using 2 stacks and a max_stack.
+    O(n) solution to the maximum sliding window problem that seeks to find the
+    largest element in all contiguous subarrays of lenght k. Method uses dequeue
+    and therefore O(k) extra space.
     """
-    queue = Queue()
-    for num in arr[:k]:
-        queue.enqueue(num)
-    queue.print_max()
-    num = k
-    for window in range(n-k):
-        queue.dequeue()
-        queue.enqueue(arr[num])
-        queue.print_max()
-        num += 1
+    q = deque()
+    for i in range(k):
+        while q and arr[q[-1]] < arr[i]:
+            q.pop()
+        q.append(i)
+
+    for i in range(k, n):
+        print(str(arr[q[0]]) + ' ', end='')
+        while q and q[0] <= i - k:
+            q.popleft()
+        while q and arr[i] >= arr[q[-1]]:
+            q.pop()
+        q.append(i)
+    print(str(arr[q[0]]))
 
 
 if __name__ == '__main__':
