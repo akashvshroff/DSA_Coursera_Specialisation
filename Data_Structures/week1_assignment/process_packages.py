b7eb58532299a525a5420f220ce57ec802c9de3e
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-08-25 14:05:45 +0530

    Week 1 - Corrected

--- Data_Structures/week1_assignment/process_packages.py
+++ Data_Structures/week1_assignment/process_packages.py
@@ -1,66 +1,35 @@
-class Request:
-    def __init__(self, arrival_time, process_time):
-        self.arrival_time = arrival_time
-        self.process_time = process_time
+from collections import deque
 
 
-class Response:
-    def __init__(self, dropped, start_time):
-        self.dropped = dropped
-        self.start_time = start_time
+def process_packets(packets, bufsize):
+    # Stores the scheduled finish times for packets
+    buffer = deque(maxlen=bufsize)
 
+    start_times = [None] * len(packets)
+    for i, (arrival, duration) in enumerate(packets):
+        # Remove packets from the buffer that have been processed by the
+        # arrival time.
+        while buffer and buffer[0] <= arrival:
+            buffer.popleft()
 
-class Buffer:
-    def __init__(self, size):
-        self.size = size
-        self.finish_time = []
-
-    def Process(self, request):
-        # remove all finished job (has finish_time < current_request.start_time
-        while len(self.finish_time) > 0 and self.finish_time[0] <= request.arrival_time:
-            self.finish_time.pop(0)
-
-        if len(self.finish_time) < self.size:
-            if len(self.finish_time) == 0:
-                self.finish_time.append(request.arrival_time + request.process_time)
-                return Response(True, request.arrival_time)
-            else:
-                start_time = request.arrival_time
-                if self.finish_time[-1] > start_time:
-                    start_time = self.finish_time[-1]
-                elif self.finish_time[-1] == start_time:
-                    start_time = self.finish_time[-1] + 1
-                self.finish_time.append(start_time + request.process_time)
-                return Response(True, start_time)
+        if len(buffer) >= bufsize:
+            # Buffer overrun
+            start_times[i] = -1
         else:
-            return Response(False, -1)
+            # This packet will start being processed after the finish time of
+            # the last buffered packet (if there is anything in the buffer).
+            start_times[i] = max(arrival, buffer[-1] if buffer else 0)
+
+            # Store the scheduled finish time for this packet.
+            buffer.append(start_times[i] + duration)
+    return start_times
 
 
-def ReadRequests(count):
-    requests = []
-    for i in range(count):
-        arrival_time, process_time = map(int, input().strip().split())
-        requests.append(Request(arrival_time, process_time))
-    return requests
-
-
-def ProcessRequests(requests, buffer):
-    responses = []
-    for request in requests:
-        responses.append(buffer.Process(request))
-    return responses
-
-
-def PrintResponses(responses):
-    for response in responses:
-        print(response.start_time if response.dropped else -1)
-
-
-if __name__ == "__main__":
-    size, count = map(int, input().strip().split())
-    requests = ReadRequests(count)
-
-    buffer = Buffer(size)
-    responses = ProcessRequests(requests, buffer)
-
-    PrintResponses(responses)
+if __name__ == '__main__':
+    s, n = list(map(int, input().split()))
+    packets = []
+    for i in range(n):
+        packets.append(list(map(int, input().split())))
+    times = process_packets(packets, s)
+    for time in times:
+        print(time)
