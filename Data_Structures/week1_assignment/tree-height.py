f1a1e1286a9efde7b9bdbd7437098302ad120aca
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-08-25 10:25:52 +0530

    Week 1 - Partial

--- Data_Structures/week1_assignment/tree-height.py
+++ Data_Structures/week1_assignment/tree-height.py
@@ -6,28 +6,45 @@ sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
 
 
-class TreeHeight:
-    def read(self):
-        self.n = int(sys.stdin.readline())
-        self.parent = list(map(int, sys.stdin.readline().split()))
+class Node:
+    """
+    Helper function to create tree nodes.
+    """
 
-    def compute_height(self):
-        # Replace this code with a faster implementation
-        maxHeight = 0
-        for vertex in range(self.n):
-            height = 0
-            i = vertex
-            while i != -1:
-                height += 1
-                i = self.parent[i]
-            maxHeight = max(maxHeight, height)
-        return maxHeight
+    def __init__(self, key=None):
+        self.key = key
+        self.children = []
+
+
+def build_tree(n, node_data):
+    """
+    Given info about nodes, build a tree.
+    """
+    nodes = [Node(i) for i in range(n)]
+    root = None
+    for child in range(n):
+        parent = node_data[child]
+        if parent == -1:
+            root = child
+        else:
+            nodes[parent].children.append(nodes[child])
+    # print(nodes)
+    return find_height(root, nodes)
+
+
+def find_height(root, nodes):
+    """
+    Find the height of a given tree using recursion.
+    """
+    if not nodes:
+        return 0
+    return 1 + max(find_height(child, child.children) for child in nodes)
 
 
 def main():
-    tree = TreeHeight()
-    tree.read()
-    print(tree.compute_height())
+    n = int(input())
+    node_data = list(map(int, input().split()))
+    print(build_tree(n, node_data))
 
 
 threading.Thread(target=main).start()
