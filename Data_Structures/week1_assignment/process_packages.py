eae59acc50eaf1c67b5bba27c61f52c6e735f8bd
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-08-25 13:57:10 +0530

    Week 1

--- Data_Structures/week1_assignment/process_packages.py
+++ Data_Structures/week1_assignment/process_packages.py
@@ -1,9 +1,13 @@
-# python3
+class Request:
+    def __init__(self, arrival_time, process_time):
+        self.arrival_time = arrival_time
+        self.process_time = process_time
 
-from collections import namedtuple
 
-Request = namedtuple("Request", ["arrived_at", "time_to_process"])
-Response = namedtuple("Response", ["was_dropped", "started_at"])
+class Response:
+    def __init__(self, dropped, start_time):
+        self.dropped = dropped
+        self.start_time = start_time
 
 
 class Buffer:
@@ -11,31 +15,52 @@ class Buffer:
         self.size = size
         self.finish_time = []
 
-    def process(self, request):
-        # write your code here
-        return Response(False, -1)
+    def Process(self, request):
+        # remove all finished job (has finish_time < current_request.start_time
+        while len(self.finish_time) > 0 and self.finish_time[0] <= request.arrival_time:
+            self.finish_time.pop(0)
+
+        if len(self.finish_time) < self.size:
+            if len(self.finish_time) == 0:
+                self.finish_time.append(request.arrival_time + request.process_time)
+                return Response(True, request.arrival_time)
+            else:
+                start_time = request.arrival_time
+                if self.finish_time[-1] > start_time:
+                    start_time = self.finish_time[-1]
+                elif self.finish_time[-1] == start_time:
+                    start_time = self.finish_time[-1] + 1
+                self.finish_time.append(start_time + request.process_time)
+                return Response(True, start_time)
+        else:
+            return Response(False, -1)
 
 
-def process_requests(requests, buffer):
+def ReadRequests(count):
+    requests = []
+    for i in range(count):
+        arrival_time, process_time = map(int, input().strip().split())
+        requests.append(Request(arrival_time, process_time))
+    return requests
+
+
+def ProcessRequests(requests, buffer):
     responses = []
     for request in requests:
-        responses.append(buffer.process(request))
+        responses.append(buffer.Process(request))
     return responses
 
 
-def main():
-    buffer_size, n_requests = map(int, input().split())
-    requests = []
-    for _ in range(n_requests):
-        arrived_at, time_to_process = map(int, input().split())
-        requests.append(Request(arrived_at, time_to_process))
-
-    buffer = Buffer(buffer_size)
-    responses = process_requests(requests, buffer)
-
+def PrintResponses(responses):
     for response in responses:
-        print(response.started_at if not response.was_dropped else -1)
+        print(response.start_time if response.dropped else -1)
 
 
 if __name__ == "__main__":
-    main()
+    size, count = map(int, input().strip().split())
+    requests = ReadRequests(count)
+
+    buffer = Buffer(size)
+    responses = ProcessRequests(requests, buffer)
+
+    PrintResponses(responses)
