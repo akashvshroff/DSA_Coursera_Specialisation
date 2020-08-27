72276e4ab0fecca1bd6dba056ea57f414dc7fe78
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-08-27 14:50:19 +0530

    Week 3 - Partial

--- Data_Structures/week3_assignment/build_heap.py
+++ Data_Structures/week3_assignment/build_heap.py
@@ -1,24 +1,44 @@
 # python3
 
 
-def build_heap(data):
-    """Build a heap from ``data`` inplace.
+def left_child(i): return 2*i
 
+
+def right_child(i): return 2*i+1
+
+
+def sift_down(arr, i, size):
+    """
+    Bubbles down a value in the min-heap if its child is smaller than it. Swaps
+    a parent node with the min of its child nodes and counts num swaps.
+    """
+    swaps = []
+    max_size = None
+    while True:
+        max_size = i
+        l = left_child(i)
+        r = right_child(i)
+        if l <= size and arr[l] < arr[max_size]:
+            max_size = l
+        if r <= size and arr[r] < arr[max_size]:
+            max_size = r
+        if i != max_size:
+            swaps.append([i, max_size])
+            arr[i], arr[max_size] = arr[max_size], arr[i]
+        else:
+            break
+    return swaps
+
+
+def build_heap(data):
+    """
+    Build a heap from ``data`` inplace.
     Returns a sequence of swaps performed by the algorithm.
     """
-    # The following naive implementation just sorts the given sequence
-    # using selection sort algorithm and saves the resulting sequence
-    # of swaps. This turns the given array into a heap, but in the worst
-    # case gives a quadratic number of swaps.
-    #
-    # TODO: replace by a more efficient implementation
     swaps = []
-    for i in range(len(data)):
-        for j in range(i + 1, len(data)):
-            if data[i] > data[j]:
-                swaps.append((i, j))
-                data[i], data[j] = data[j], data[i]
-    return swaps
+    n = len(data)-1
+    for i in range(n//2, 0, -1):
+        swaps.extend(sift_down(data, i, n))
 
 
 def main():
