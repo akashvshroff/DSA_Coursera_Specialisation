b3add96ced0d81d8fb5360b3b6892006d7308e73
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-08-27 15:11:46 +0530

    Week 3 - Partial

--- Data_Structures/week3_assignment/build_heap.py
+++ Data_Structures/week3_assignment/build_heap.py
@@ -1,32 +1,28 @@
 # python3
 
 
-def left_child(i): return 2*i
+def left_child(i): return 2*i+1
 
 
-def right_child(i): return 2*i+1
+def right_child(i): return 2*i+2
 
 
-def sift_down(arr, i, size):
+def sift_down(arr, i, size, swaps=[]):
     """
     Bubbles down a value in the min-heap if its child is smaller than it. Swaps
     a parent node with the min of its child nodes and counts num swaps.
     """
-    swaps = []
-    max_size = None
-    while True:
-        max_size = i
-        l = left_child(i)
-        r = right_child(i)
-        if l <= size and arr[l] < arr[max_size]:
-            max_size = l
-        if r <= size and arr[r] < arr[max_size]:
-            max_size = r
-        if i != max_size:
-            swaps.append([i, max_size])
-            arr[i], arr[max_size] = arr[max_size], arr[i]
-        else:
-            break
+    max_size = i
+    l = left_child(i)
+    r = right_child(i)
+    if l <= size and arr[l] < arr[max_size]:
+        max_size = l
+    if r <= size and arr[r] < arr[max_size]:
+        max_size = r
+    if i != max_size:
+        swaps.append([i, max_size])
+        arr[i], arr[max_size] = arr[max_size], arr[i]
+        sift_down(arr, max_size, size, swaps)
     return swaps
 
 
@@ -37,8 +33,10 @@ def build_heap(data):
     """
     swaps = []
     n = len(data)-1
-    for i in range(n//2, 0, -1):
-        swaps.extend(sift_down(data, i, n))
+    for i in range(n//2, -1, -1):
+        # print(i)
+        swaps.extend(sift_down(data, i, n, []))
+    return swaps
 
 
 def main():
@@ -51,6 +49,7 @@ def main():
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
+    print(data)
 
 
 if __name__ == "__main__":
