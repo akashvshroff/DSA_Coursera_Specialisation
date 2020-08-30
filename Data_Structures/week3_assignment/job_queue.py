c193e988eff8d57c45a1a6c75e6874d7af063ee0
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-08-30 22:12:19 +0530

    Week 3 - Partial

--- Data_Structures/week3_assignment/job_queue.py
+++ Data_Structures/week3_assignment/job_queue.py
@@ -10,7 +10,7 @@ def right_child(i): return i*2+2
 def sift_down(arr, i, size):
     """
     Bubbles down a value in the min-heap if its child is smaller than it. Swaps
-    a parent node with the min of its child nodes and counts num swaps.
+    a parent node with the min of its child nodes.
     """
     max_size = i
     l = left_child(i)
@@ -28,8 +28,8 @@ def sift_down(arr, i, size):
         cur_priority = arr[max_size][1]
         if r_priority < cur_priority:
             max_size = r
-        elif r_priority == cur_priority:
-            if arr[r][0] < arr[max_size][0]:
+        elif r_priority == cur_priority:  # same priority
+            if arr[r][0] < arr[max_size][0]:  # lower index
                 max_size = r
     if i != max_size:
         arr[i], arr[max_size] = arr[max_size], arr[i]
@@ -37,11 +37,24 @@ def sift_down(arr, i, size):
 
 
 def change_priority(arr, i, p):
+    """
+    Adds the time taken for a job and bubbles the node down.
+    """
     arr[i][1] += p
     arr = sift_down(arr, i, len(arr)-1)
 
 
 def assign_jobs(n_workers, jobs):
+    """
+    Program that simulates parallel processing in a program. Given a list of
+    jobs where the ith index represents the time taken for the ith job.
+    A min heap is used to make the program run at O(n) and the heap stores the
+    worker id(index number) and next free time. At each stage the worker with
+    the minimum next free time is chosen and if 2 workers are free
+    the one with the lower index is given priority as the workers get the jobs
+    chronologically. Workers are from 0 to n-1 where n is the number of workers.
+    Output the worker chosen and time job started for each job.
+    """
     min_heap = [[i, 0] for i in range(n_workers)]
     workers = []
     for job in jobs:
