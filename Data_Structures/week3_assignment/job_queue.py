3d219898b2e3911f8ac782aba4bbe62a5811c02a
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-08-30 22:01:03 +0530

    Week 3 - Partial

--- Data_Structures/week3_assignment/job_queue.py
+++ Data_Structures/week3_assignment/job_queue.py
@@ -15,10 +15,19 @@ def sift_down(arr, i, size):
     max_size = i
     l = left_child(i)
     r = right_child(i)
-    if l <= size and arr[l][1] < arr[max_size][1]:
-        max_size = l
-    if r <= size and arr[r][1] < arr[max_size][1]:
-        max_size = r
+    if l <= size and arr[l][1] <= arr[max_size][1]:
+        if arr[l][1] == arr[max_size][1]:
+            if arr[l][0] < arr[max_size][0]:
+                max_size = l
+        else:
+            max_size = l
+    if r <= size and arr[r][1] <= arr[max_size][1]:
+        if arr[r][1] == arr[max_size][1]:
+            if arr[r][0] < arr[max_size][0]:
+                max_size = r
+        else:
+            max_size = r
+
     if i != max_size:
         arr[i], arr[max_size] = arr[max_size], arr[i]
         sift_down(arr, max_size, size)
@@ -33,6 +42,7 @@ def assign_jobs(n_workers, jobs):
     min_heap = [[i, 0] for i in range(n_workers)]
     workers = []
     for job in jobs:
+        # print(min_heap)
         workers.append(min_heap[0][::])
         curr_worker = min_heap[0]
         change_priority(min_heap, 0, job)
