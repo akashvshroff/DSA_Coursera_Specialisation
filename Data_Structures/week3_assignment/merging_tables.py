a9dfb8db471d82cad7a38c14e8504d2331c3b7f9
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-08-31 11:38:29 +0530

    Week 3

--- Data_Structures/week3_assignment/merging_tables.py
+++ Data_Structures/week3_assignment/merging_tables.py
@@ -6,7 +6,7 @@ class Database:
         self.row_counts = row_counts
         self.max_row_count = max(row_counts)
         n_tables = len(row_counts)
-        self.ranks = [1] * n_tables
+        self.ranks = [0] * n_tables
         self.parents = list(range(n_tables))
 
     def merge(self, src, dst):
@@ -14,15 +14,23 @@ class Database:
         dst_parent = self.get_parent(dst)
 
         if src_parent == dst_parent:
-            return False
-
-        # merge two components
-        # use union by rank heuristic
-        # update max_row_count with the new maximum table size
+            return
+        if self.ranks[src_parent] > self.ranks[dst_parent]:
+            self.parents[dst_parent] = src_parent
+            self.row_counts[src_parent] += self.row_counts[dst_parent]
+        else:
+            self.parents[src_parent] = dst_parent
+            self.row_counts[dst_parent] += self.row_counts[src_parent]
+            if self.ranks[src_parent] == self.ranks[dst_parent]:
+                self.ranks[dst_parent] += 1
+        self.max_row_count = max(
+            self.max_row_count, self.row_counts[dst_parent], self.row_counts[src_parent])
         return True
 
     def get_parent(self, table):
         # find parent and compress path
+        if table != self.parents[table]:
+            self.parents[table] = self.get_parent(self.parents[table])
         return self.parents[table]
 
 
