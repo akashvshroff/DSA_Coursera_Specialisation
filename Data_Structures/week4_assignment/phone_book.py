d882b6de33ab7a62d675fbd9a0bd0e230751f48d
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-09-01 14:29:29 +0530

    Week 4 - Partial

--- Data_Structures/week4_assignment/phone_book.py
+++ Data_Structures/week4_assignment/phone_book.py
@@ -1,5 +1,6 @@
 # python3
 
+
 class Query:
     def __init__(self, query):
         self.type = query[0]
@@ -7,41 +8,36 @@ class Query:
         if self.type == 'add':
             self.name = query[2]
 
+
 def read_queries():
     n = int(input())
     return [Query(input().split()) for i in range(n)]
 
+
 def write_responses(result):
     print('\n'.join(result))
 
+
 def process_queries(queries):
+    """
+    A simple implementation of a phone book using the direct addressing hash
+    method. The method is memory inefficient and in some ways naive during the
+    set-up of the array of required size (here 10**7 since max phone num size
+    is 7 digits) but all other operations all occur in O(1) time.
+    """
     result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
-    for cur_query in queries:
-        if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
-        elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+    contacts = ['' for _ in range(10**7)]
+    for cur in queries:
+        if cur.type == 'add':
+            contacts[cur.number] = cur.name
+        elif cur.type == 'del':
+            contacts[cur.number] = ""
         else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
+            find_name = contacts[cur.number]
+            response = 'not found' if not find_name else find_name
             result.append(response)
     return result
 
+
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
-
