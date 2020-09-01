9685c42274163ca48d890c5dc1a13a4619c47522
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-09-01 15:08:39 +0530

    Week 4 - Partial

--- Data_Structures/week4_assignment/hash_chains.py
+++ Data_Structures/week4_assignment/hash_chains.py
@@ -19,7 +19,7 @@ class QueryProcessor:
 
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
-        self.elems = [[] for _ in range(self.bucket_count)]
+        self.buckets = [[] for _ in range(self.bucket_count)]
 
     def _hash_func(self, s):
         """
@@ -30,33 +30,52 @@ class QueryProcessor:
             ans = (ans * self._multiplier + ord(c)) % self._prime
         return ans % self.bucket_count
 
-    def write_search_result(self, was_found):
-        print('yes' if was_found else 'no')
+    def add(self, hash, s):
+        """
+        Add a string to the hash-table if it isn't in it already.
+        """
+        if s not in self.buckets[hash]:
+            self.buckets[hash].insert(0, s)
 
-    def write_chain(self, chain):
-        print(' '.join(chain))
+    def check(self, id):
+        """
+        Print all elements in a particular bucket.
+        """
+        print(' '.join(self.buckets[id]))
+
+    def find(self, hash, s):
+        """
+        Return yes if a string is in the hash-table, no if not.
+        """
+        for elem in self.buckets[hash]:
+            if elem == s:
+                print('yes')
+                return
+        print('no')
+
+    def _del(self, hash, s):
+        """
+        Remove a particular string from the hash-table.
+        """
+        try:
+            self.buckets[hash].remove(s)
+        except ValueError:
+            pass
 
     def read_query(self):
         return Query(input().split())
 
     def process_query(self, query):
-        if query.type == "check":
-            self.write_chain(self.elems[query.ind][::-1])
+        if query.type == 'check':
+            self.check(query.ind)
         else:
-            hash_ind = self._hash_func(query.s)
-            if query.type == 'find':
-                bucket = self.elems[hash_ind]
-                found = False
-                for str_ in bucket:
-                    if str_ == query.s:
-                        found = True
-                        break
-                self.write_search_result(found)
-            elif query.type == 'add':
-                self.elems[hash_ind].append(query.s)
+            hash = self._hash_func(query.s)
+            if query.type == 'add':
+                self.add(hash, query.s)
+            elif query.type == 'find':
+                self.find(hash, query.s)
             else:
-                bucket = self.elems[hash_ind]
-                self.elems[hash_ind] = [elem for elem in bucket if elem != query.s]
+                self._del(hash, query.s)
 
     def process_queries(self):
         n = int(input())
