95cee3dcd11dd1d5edd7ce9bb191f4ee5bd8d345
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-09-07 11:33:26 +0530

    Week 6 - Partial

--- Data_Structures/week6_assignment/tree-orders.py
+++ Data_Structures/week6_assignment/tree-orders.py
@@ -1,47 +1,71 @@
 # python3
 
-import sys, threading
-sys.setrecursionlimit(10**6) # max depth of recursion
+import sys
+import threading
+sys.setrecursionlimit(10**6)  # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
 
+
 class TreeOrders:
-  def read(self):
-    self.n = int(sys.stdin.readline())
-    self.key = [0 for i in range(self.n)]
-    self.left = [0 for i in range(self.n)]
-    self.right = [0 for i in range(self.n)]
-    for i in range(self.n):
-      [a, b, c] = map(int, sys.stdin.readline().split())
-      self.key[i] = a
-      self.left[i] = b
-      self.right[i] = c
+    def __init__(self):
+        self.in_order_res, self.pre_order_res, self.post_order_res = [], [], []
 
-  def inOrder(self):
-    self.result = []
-    # Finish the implementation
-    # You may need to add a new recursive method to do that
-                
-    return self.result
+    def read(self):
+        """
+        Given a tree rooted an index-0, print the pre,post and in-order traversals.
+        """
+        self.n = int(sys.stdin.readline())
+        self.key = [0 for i in range(self.n)]
+        self.left = [0 for i in range(self.n)]
+        self.right = [0 for i in range(self.n)]
+        for i in range(self.n):
+            [a, b, c] = map(int, sys.stdin.readline().split())
+            self.key[i] = a
+            self.left[i] = b
+            self.right[i] = c
+        r = 0
+        self.in_order(r)
+        self.pre_order(r)
+        self.post_order(r)
 
-  def preOrder(self):
-    self.result = []
-    # Finish the implementation
-    # You may need to add a new recursive method to do that
-                
-    return self.result
+    def in_order(self, root):
+        """
+        In order traversal of a tree - from smallest to largest node.
+        Left-Root-Right
+        """
+        if root == -1:
+            return
+        self.in_order(self.left[root])
+        self.in_order_res.append(self.key[root])
+        self.in_order(self.right[root])
 
-  def postOrder(self):
-    self.result = []
-    # Finish the implementation
-    # You may need to add a new recursive method to do that
-                
-    return self.result
+    def pre_order(self, root):
+        """
+        Root-Left-Right
+        """
+        if root == -1:
+            return
+        self.pre_order_res.append(self.key[root])
+        self.pre_order(self.left[root])
+        self.pre_order(self.right[root])
+
+    def post_order(self, root):
+        """
+        Left-Right-Root
+        """
+        if root == -1:
+            return
+        self.post_order(self.left[root])
+        self.post_order(self.right[root])
+        self.post_order_res.append(self.key[root])
+
 
 def main():
-	tree = TreeOrders()
-	tree.read()
-	print(" ".join(str(x) for x in tree.inOrder()))
-	print(" ".join(str(x) for x in tree.preOrder()))
-	print(" ".join(str(x) for x in tree.postOrder()))
+    tree = TreeOrders()
+    tree.read()
+    print(" ".join(map(str, tree.in_order_res)))
+    print(" ".join(map(str, tree.pre_order_res)))
+    print(" ".join(map(str, tree.post_order_res)))
+
 
 threading.Thread(target=main).start()
