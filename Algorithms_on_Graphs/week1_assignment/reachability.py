4c776c2b2e36faf214f77935801f34a1b33d23d6
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-10-01 14:26:54 +0530

    Week 1 - Partial

--- Algorithms_on_Graphs/week1_assignment/reachability.py
+++ Algorithms_on_Graphs/week1_assignment/reachability.py
@@ -1,10 +1,28 @@
-#Uses python3
+# Uses python3
 
 import sys
 
-def reach(adj, x, y):
-    #write your code here
-    return 0
+
+def explore(v, visited, adj):
+    """
+    Checks all the neighbours v in a recursive algorithm and sets all the nodes
+    that you can visit to true.
+    """
+    visited[v] = True
+    for w in adj[v]:
+        if not visited[w]:
+            visited = explore(w, visited, adj)
+    return visited
+
+
+def reach(adj, x, y, n):
+    """
+    Initialises a hashmap that checks reachability and returns if reachable.
+    """
+    visited = {i: False for i in range(n)}
+    visited = explore(x, visited, adj)
+    return 1 if visited[y] else 0
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -18,4 +36,4 @@ if __name__ == '__main__':
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
-    print(reach(adj, x, y))
+    print(reach(adj, x, y, n))
