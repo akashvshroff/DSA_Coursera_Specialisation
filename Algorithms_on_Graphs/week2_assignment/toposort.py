56e37c06baa0b05a0075bb46eca83ca052c2d859
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-10-02 11:38:14 +0530

    Week 2 Partial

--- Algorithms_on_Graphs/week2_assignment/toposort.py
+++ Algorithms_on_Graphs/week2_assignment/toposort.py
@@ -1,17 +1,46 @@
-#Uses python3
+# Uses python3
 
 import sys
 
-def dfs(adj, used, order, x):
-    #write your code here
-    pass
 
+class TopoSort:
+    def __init__(self, adj, n):
+        self.adj = adj
+        self.visited = {i: False for i in range(n)}
+        self.n = n
+        self.clock = 1
+        self.previsit = [0]*n
+        self.postvisit = [0]*n
 
-def toposort(adj):
-    used = [0] * len(adj)
-    order = []
-    #write your code here
-    return order
+    def pre(self, v):
+        self.previsit[v] = self.clock
+        self.clock += 1
+
+    def post(self, v):
+        self.postvisit[v] = self.clock
+        self.clock += 1
+
+    def explore(self, v):
+        self.visited[v] = True
+        self.pre(v)
+        for vertex in self.adj[v]:
+            if not self.visited[vertex]:
+                self.explore(vertex)
+        self.post(v)
+
+    def dfs(self):
+        for vertex in range(self.n):
+            if not self.visited[vertex]:
+                self.explore(vertex)
+
+    def toposort(self):
+        """
+        Returns nodes in a linear, topological ordering.
+        """
+        self.dfs()
+        nodes_sorted = sorted(range(1, n+1), key=lambda x: self.postvisit[x-1], reverse=True)
+        return map(str, nodes_sorted)
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -22,7 +51,5 @@ if __name__ == '__main__':
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
-    order = toposort(adj)
-    for x in order:
-        print(x + 1, end=' ')
-
+    order = TopoSort(adj, n)
+    print(' '.join(order.toposort()))
