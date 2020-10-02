5594d2f147b488b7de9d25547d87a98dfd621593
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-10-02 11:32:24 +0530

    Week 2 Partial

--- Algorithms_on_Graphs/week2_assignment/acyclicity.py
+++ Algorithms_on_Graphs/week2_assignment/acyclicity.py
@@ -1,10 +1,46 @@
-#Uses python3
+# Uses python3
 
 import sys
 
 
-def acyclic(adj):
-    return 0
+class Acyclic:
+    def __init__(self, adj, n):
+        self.adj = adj
+        self.visited = {i: False for i in range(n)}
+        self.n = n
+        self.clock = 1
+        self.previsit = [0]*n
+        self.postvisit = [0]*n
+
+    def pre(self, v):
+        self.previsit[v] = self.clock
+        self.clock += 1
+
+    def post(self, v):
+        self.postvisit[v] = self.clock
+        self.clock += 1
+
+    def explore(self, v):
+        self.visited[v] = True
+        self.pre(v)
+        for vertex in self.adj[v]:
+            if not self.visited[vertex]:
+                self.explore(vertex)
+        self.post(v)
+
+    def dfs(self):
+        for vertex in range(self.n):
+            if not self.visited[vertex]:
+                self.explore(vertex)
+
+    def acyclic(self):
+        self.dfs()
+        for u in range(n):
+            for v in self.adj[u]:
+                if not self.postvisit[u] > self.postvisit[v]:
+                    return 1
+        return 0
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -15,4 +51,5 @@ if __name__ == '__main__':
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
-    print(acyclic(adj))
+    obj = Acyclic(adj, n)
+    print(obj.acyclic())
