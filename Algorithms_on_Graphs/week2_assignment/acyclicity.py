6f30019a18a8622f68d5d5768f394f626983714a
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-10-02 12:20:24 +0530

    Week 2 Partial

--- Algorithms_on_Graphs/week2_assignment/acyclicity.py
+++ Algorithms_on_Graphs/week2_assignment/acyclicity.py
@@ -7,37 +7,32 @@ class Acyclic:
     def __init__(self, adj, n):
         self.adj = adj
         self.visited = {i: False for i in range(n)}
+        self.stack = {k: False for k in range(n)}  # true if in stack
         self.n = n
         self.clock = 1
-        self.previsit = [0]*n
-        self.postvisit = [0]*n
-
-    def pre(self, v):
-        self.previsit[v] = self.clock
-        self.clock += 1
-
-    def post(self, v):
-        self.postvisit[v] = self.clock
-        self.clock += 1
 
     def explore(self, v):
+        """
+        Explores all the nodes that are reachable from v. Uses a pseudo-stack
+        to see if any node that has already been visited is being visited again.
+        """
         self.visited[v] = True
-        self.pre(v)
+        self.stack[v] = True
+        # self.pre(v)
         for vertex in self.adj[v]:
             if not self.visited[vertex]:
-                self.explore(vertex)
-        self.post(v)
+                if self.explore(vertex):
+                    return True
+            elif self.stack[vertex]:
+                return True
+        # self.post(v)
+        self.stack[v] = False  # popping stack
+        return False
 
     def dfs(self):
         for vertex in range(self.n):
             if not self.visited[vertex]:
-                self.explore(vertex)
-
-    def acyclic(self):
-        self.dfs()
-        for u in range(n):
-            for v in self.adj[u]:
-                if not self.postvisit[u] > self.postvisit[v]:
+                if self.explore(vertex):
                     return 1
         return 0
 
@@ -52,4 +47,4 @@ if __name__ == '__main__':
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
     obj = Acyclic(adj, n)
-    print(obj.acyclic())
+    print(obj.dfs())
