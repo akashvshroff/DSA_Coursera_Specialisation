46dc9521a89c61243511276828284837c5711936
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-10-06 11:35:10 +0530

    Week 4 Partial

--- Algorithms_on_Graphs/week4_assignment/dijkstra.py
+++ Algorithms_on_Graphs/week4_assignment/dijkstra.py
@@ -1,12 +1,31 @@
-#Uses python3
+# Uses python3
 
 import sys
-import queue
+import heapq
 
 
-def distance(adj, cost, s, t):
-    #write your code here
-    return -1
+def distance(adj, cost, s, t, n):
+    """
+    An implementation of Dijkstra's algorithm in order to find the shortest
+    path between 2 nodes based on some weight attributed to each edge.
+    """
+    dist = [float("inf") for _ in range(n)]
+    prev = [None for _ in range(n)]
+    dist[s] = 0
+    heap = [(dist[i], i) for i in range(n)]
+    heapq.heapify(heap)
+    min_checked = {i: False for i in range(n)}  # checks if the min has already been found
+    while heap:
+        u = heapq.heappop(heap)  # min id tuple
+        d, id = u
+        if not min_checked[id]:
+            for cost_id, vertex in enumerate(adj[id]):
+                if dist[vertex] > d + cost[id][cost_id]:
+                    dist[vertex] = d + cost[id][cost_id]
+                    prev[vertex] = id
+                    heapq.heappush(heap, (dist[vertex], vertex))
+        min_checked[id] = True  # so that it cannot be checked again
+    return dist[t] if dist[t] != float('inf') else -1
 
 
 if __name__ == '__main__':
@@ -22,4 +41,4 @@ if __name__ == '__main__':
         adj[a - 1].append(b - 1)
         cost[a - 1].append(w)
     s, t = data[0] - 1, data[1] - 1
-    print(distance(adj, cost, s, t))
+    print(distance(adj, cost, s, t, n))
