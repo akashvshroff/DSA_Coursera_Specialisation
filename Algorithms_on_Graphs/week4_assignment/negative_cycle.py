7ec1464c471098e0aa11176c44c67dfc9f6a7a22
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-10-07 12:20:01 +0530

    Week 4 Partial

--- Algorithms_on_Graphs/week4_assignment/negative_cycle.py
+++ Algorithms_on_Graphs/week4_assignment/negative_cycle.py
@@ -10,7 +10,7 @@ def relax(u, v, dist, c):
     return False
 
 
-def negative_cycle(adj, cost, n):
+def bellman_ford(adj, cost, n, s):
     """
     Bellman-Ford algorithm to detect a negative weight cycle in a graph by
     relaxing edges and checking if an edge relaxed on the Vth iteration where
@@ -18,17 +18,17 @@ def negative_cycle(adj, cost, n):
     """
     dist = [float('inf') for _ in range(n)]
     prev = [None for _ in range(n)]
-    dist[0] = 0
+    dist[s] = 0
     for iteration in range(n-1):
         for u in range(n):
             for id, vertex in enumerate(adj[u]):
                 relax(u, vertex, dist, cost[u][id])
     # final iteration
-    found = False
     for u in range(n):
         for id, vertex in enumerate(adj[u]):
-            found = relax(u, v, dist, cost[u][id])
-    return 1 if found else 0
+            if relax(u, vertex, dist, cost[u][id]):
+                return 1
+    return 0
 
 
 if __name__ == '__main__':
@@ -38,9 +38,11 @@ if __name__ == '__main__':
     data = data[2:]
     edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
     data = data[3 * m:]
-    adj = [[] for _ in range(n)]
-    cost = [[] for _ in range(n)]
+    adj = [[] for _ in range(n+1)]
+    cost = [[] for _ in range(n+1)]
+    adj[0] = [i for i in range(1, n+1)]
+    cost[0] = [i for i in range(1, n+1)]
     for ((a, b), w) in edges:
-        adj[a - 1].append(b - 1)
-        cost[a - 1].append(w)
-    print(negative_cycle(adj, cost, n))
+        adj[a].append(b)
+        cost[a].append(w)
+    print(bellman_ford(adj, cost, n+1, 0))
