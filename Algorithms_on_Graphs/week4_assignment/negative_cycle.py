0ca17643bc114aa3658f59497b821129bc7c6033
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-10-07 11:11:33 +0530

    Week 4 Partial

--- Algorithms_on_Graphs/week4_assignment/negative_cycle.py
+++ Algorithms_on_Graphs/week4_assignment/negative_cycle.py
@@ -1,11 +1,34 @@
-#Uses python3
+# Uses python3
 
 import sys
 
 
-def negative_cycle(adj, cost):
-    #write your code here
-    return 0
+def relax(u, v, dist, c):
+    if dist[v] > dist[u] + c:
+        dist[v] = dist[u] + c
+        return True
+    return False
+
+
+def negative_cycle(adj, cost, n):
+    """
+    Bellman-Ford algorithm to detect a negative weight cycle in a graph by
+    relaxing edges and checking if an edge relaxed on the Vth iteration where
+    V is the number of nodes in the graph.
+    """
+    dist = [float('inf') for _ in range(n)]
+    prev = [None for _ in range(n)]
+    dist[0] = 0
+    for iteration in range(n-1):
+        for u in range(n):
+            for id, vertex in enumerate(adj[u]):
+                relax(u, vertex, dist, cost[u][id])
+    # final iteration
+    found = False
+    for u in range(n):
+        for id, vertex in enumerate(adj[u]):
+            found = relax(u, v, dist, cost[u][id])
+    return 1 if found else 0
 
 
 if __name__ == '__main__':
@@ -20,4 +43,4 @@ if __name__ == '__main__':
     for ((a, b), w) in edges:
         adj[a - 1].append(b - 1)
         cost[a - 1].append(w)
-    print(negative_cycle(adj, cost))
+    print(negative_cycle(adj, cost, n))
