98ecdc80cfa2379341f33ca5240a1e5bbd03c172
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-10-28 22:27:09 +0530

    Week 2 Partial

--- Algorithms_on_Strings/week2_assigment/bwtinverse.py
+++ Algorithms_on_Strings/week2_assigment/bwtinverse.py
@@ -1,9 +1,37 @@
 # python3
 import sys
 
+
 def InverseBWT(bwt):
-    # write your code here
-    return ""
+    """
+    Given the Burrow-Wheeler Transform of a string, return the
+    original string efficiently by employing the First-Last
+    property of the BWT.
+    """
+    hash = {}
+    last_col = []
+    hash_1 = {}
+    first_col = []
+    bwt_asc = sorted(bwt)
+    for i in range(len(bwt)):
+        ch = bwt[i]
+        if hash.get(ch, None) is None:
+            hash[ch] = 0
+        last_col.append('{}{}'.format(ch, hash[ch]))
+        hash[ch] += 1
+        ch = bwt_asc[i]
+        if hash_1.get(ch, None) is None:
+            hash_1[ch] = 0
+        first_col.append('{}{}'.format(ch, hash_1[ch]))
+        hash_1[ch] += 1
+
+    s = '$'
+    cur = 0
+    while len(s) != len(bwt):
+        char = last_col[cur]
+        s = char[0] + s
+        cur = first_col.index(char)
+    return s
 
 
 if __name__ == '__main__':
