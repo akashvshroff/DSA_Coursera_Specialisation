4fb9e0832bd858d6017b4c762d45dcc9829cf824
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-10-28 21:49:00 +0530

    Week 2 Partial

--- Algorithms_on_Strings/week2_assigment/bwt.py
+++ Algorithms_on_Strings/week2_assigment/bwt.py
@@ -1,8 +1,21 @@
 # python3
 import sys
 
+
 def BWT(text):
-    return ""
+    """
+    Return the Burrow-Wheeler Transform a given string.
+    This is done by creating a list of all the cyclic rotations of
+    the string, sorting them and then fetching all the last characters.
+    """
+    rev = []
+    for i in range(len(text)):
+        if not rev:
+            rev.append(text[-1] + text[:-1])
+        else:
+            rev.append(rev[-1][-1] + rev[-1][:-1])
+    return ''.join(rs[-1] for rs in sorted(rev))
+
 
 if __name__ == '__main__':
     text = sys.stdin.readline().strip()
