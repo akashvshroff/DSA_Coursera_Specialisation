eb4792f743697dfbd224ee802bf215b78afd12dc
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-10-29 15:25:07 +0530

    Week 2 Partial

--- Algorithms_on_Strings/week2_assigment/bwtinverse.py
+++ Algorithms_on_Strings/week2_assigment/bwtinverse.py
@@ -2,35 +2,40 @@
 import sys
 
 
+def first_to_last(s):
+    """
+    For a BWT, return an array which marks the occurence number
+    for each symbol in the string s. Starts with -1 since
+    occurences are 0-based.
+    """
+    counts = {'$': 0, "A": 0, 'C': 0, 'G': 0, 'T': 0}
+    for char in s:
+        counts[char] += 1
+    c = {'$': 0}
+    sum_so_far = counts['$']
+    for char in ['A', 'C', 'G', 'T']:
+        c[char] = sum_so_far
+        sum_so_far += counts[char]
+    arr = [0] * len(s)
+    for i in range(len(s) - 1, -1, -1):
+        arr[i] = counts[s[i]] + c[s[i]] - 1
+        counts[s[i]] -= 1
+    return arr
+
+
 def InverseBWT(bwt):
     """
     Given the Burrow-Wheeler Transform of a string, return the
     original string efficiently by employing the First-Last
     property of the BWT.
     """
-    hash = {}
-    last_col = []
-    hash_1 = {}
-    first_col = []
-    bwt_asc = sorted(bwt)
-    for i in range(len(bwt)):
-        ch = bwt[i]
-        if hash.get(ch, None) is None:
-            hash[ch] = 0
-        last_col.append('{}{}'.format(ch, hash[ch]))
-        hash[ch] += 1
-        ch = bwt_asc[i]
-        if hash_1.get(ch, None) is None:
-            hash_1[ch] = 0
-        first_col.append('{}{}'.format(ch, hash_1[ch]))
-        hash_1[ch] += 1
-
-    s = '$'
+    f2l = first_to_last(bwt)
+    print(f2l)
     cur = 0
-    while len(s) != len(bwt):
-        char = last_col[cur]
-        s = char[0] + s
-        cur = first_col.index(char)
+    s = '$'
+    for _ in range(1, len(bwt)):
+        s = bwt[cur] + s
+        cur = f2l[cur]
     return s
 
 
